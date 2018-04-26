92fb8738e4d3100c09b435c29b654a4f4049298e
Keyur joshi <keyurjoshi@Keyurs-MacBook-Air.local>
2018-04-26 22:22:40 +0530

    keystore added. Configuration for https added

--- src/main/java/com/techcielo/sprhn/controller/CategoryController.java
+++ src/main/java/com/techcielo/sprhn/controller/CategoryController.java
@@ -5,6 +5,8 @@ import com.techcielo.sprhn.repository.CategoryRepo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +25,13 @@ public class CategoryController {
     @RequestMapping(value = "/category", method = RequestMethod.GET)
     public List<CategoryBean> getAllCategories(){
         logger.info("Getting full category list");
+        return (List<CategoryBean>) repo.findAll();
+    }
+
+    @RequestMapping(value = "/category", method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public List<CategoryBean> createCategory(@RequestBody CategoryBean bean){
+        logger.info("Creating new Category");
+        repo.save(bean);
         return (List<CategoryBean>) repo.findAll();
     }
 }
